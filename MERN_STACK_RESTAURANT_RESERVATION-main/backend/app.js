eca4fc5cc00cc87125b76317e643c6c8a38cad40
SANTOSHLADI <119658385+SANTOSHLADI@users.noreply.github.com>
2024-05-01 17:57:41 +0530

    Add files via upload

---
+++ MERN_STACK_RESTAURANT_RESERVATION-main/backend/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
+import { dbConnection } from "./database/dbConnection.js";
+
+const app = express();
+dotenv.config({ path: "./config.env" });
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST","GET"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", reservationRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
